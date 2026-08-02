766e367a4f9226ae3fe43a30748981ddbe237150
xmskyload-droid <xmskyload@gmail.com>
2026-08-02 12:33:57 +0530

    Navigation: register login route in root Stack layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout() {
       <SafeAreaProvider>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index" />
+          <Stack.Screen name="login" />
           <Stack.Screen name="onboarding" />
           <Stack.Screen name="(tabs)" />
           <Stack.Screen name="add-expense" options={{ presentation: 'modal' }} />
